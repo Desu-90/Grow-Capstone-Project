43173be46d6286c117d95e504508f295b7393e99
Dennis Olmedo (RIT Student) <dao9631@rit.edu>
2023-03-23 10:45:41 -0400

    Drag and drop almost done! Need to clone element that is being used

--- src/canvas.js
+++ src/canvas.js
@@ -35,7 +35,8 @@ let poppy = document.querySelector("#poppy");
 function dragElement(elmnt) {
     let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
+    elmnt.onpointerdown = dragMouseDown;
+    let count = [0];
 
     function dragMouseDown(e) {
         e = e || window.event;
@@ -43,9 +44,12 @@ function dragElement(elmnt) {
         // get the mouse cursor position at startup:
         pos3 = e.clientX;
         pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
+        document.onpointerup = closeDragElement;
         // call a function whenever the cursor moves:
-        document.onmousemove = elementDrag;
+        document.onpointermove = elementDrag;
+        console.log('mouseDown');
+        count[0]++;
+        console.log(count);
     }
 
     function elementDrag(e) {
@@ -53,37 +57,43 @@ function dragElement(elmnt) {
         e.preventDefault();
         console.log("grabbed");
         elmnt.style.width = '150px';
-        switch (elmnt) {
-            case rose:
-                elmnt.innerHTML = '<img src="../assets/rose.png" id="item">';
-                break;
-            case tulip:
-                elmnt.innerHTML = '<img src="../assets/tulip.png" id="item">';
-                break;
-            case daisy:
-                elmnt.innerHTML = '<img src="../assets/daisy.png" id="item">';
-                break;
-            case lily:
-                elmnt.innerHTML = '<img src="../assets/lily.png" id="item">';
-                break;
-            case poppy:
-                elmnt.innerHTML = '<img src="../assets/poppy.png" id="item">';
-                break;
-        }
+
         // calculate the new cursor position:
         pos1 = pos3 - e.clientX;
         pos2 = pos4 - e.clientY;
         pos3 = e.clientX;
         pos4 = e.clientY;
+        // console.log(`pos3: ${pos3}`)
         // set the element's new position:
         elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
         elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        if (pos3 >= 630) {
+            switch (elmnt) {
+                case rose:
+                    elmnt.innerHTML = '<img src="../assets/rose.png" id="item">';
+                    break;
+                case tulip:
+                    elmnt.innerHTML = '<img src="../assets/tulip.png" id="item">';
+                    break;
+                case daisy:
+                    elmnt.innerHTML = '<img src="../assets/daisy.png" id="item">';
+                    break;
+                case lily:
+                    elmnt.innerHTML = '<img src="../assets/lily.png" id="item">';
+                    break;
+                case poppy:
+                    elmnt.innerHTML = '<img src="../assets/poppy.png" id="item">';
+                    break;
+            }
+        }
     }
 
     function closeDragElement() {
         // stop moving when mouse button is released:
-        document.onmouseup = null;
-        document.onmousemove = null;
+        document.onpointerup = null;
+        document.onpointermove = null;
+        const canvas = document.querySelector('#canvas');
+        canvas.append(elmnt);
         // const item = document.createElement();
         // item.innerHTML = '<span id="mydiv"><img id="icon" src="../assets/rose.png"><p>Rose</p>'
         console.log("dropped");
@@ -94,8 +104,6 @@ function dragElement(elmnt) {
         // selection.parentNode.replaceChild(item, selection);
     }
 }
-
-
 dragElement(rose);
 dragElement(tulip);
 dragElement(daisy);
