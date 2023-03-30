db52df9a444d644a485d3cd91081cc6efded2cb2
Dennis Olmedo (RIT Student) <dao9631@rit.edu>
2023-03-30 10:16:42 -0400

    Cleaned up some extra files

--- src/app.js
+++ src/app.js
@@ -1,48 +1,27 @@
-
-
-// const dragElement = (e) => {
-//     let pos1 = 0, pos2 = 0, pos = 0, pos4 = 0
-//     e.onmousedown = dragMouseDown;
-// }
-
-// const dragMouseDown = (e) => {
-//     e = e || window.event;
-//     e.preventDefault();
-//     pos3 = e.clientX;
-//     pos4 = e.clientY;
-//     document.onmmouseup;
-//     document.onmousemove = elementDrag;
-// }
-
-// const elementDrag = (e) => {
-//     e = e || window.event;
-//     e.preventDefault();
-//     // calculate the new cursor position:
-//     pos1 = pos3 - e.clientX;
-//     pos2 = pos4 - e.clientY;
-//     pos3 = e.clientX;
-//     pos4 = e.clientY;
-//     // set the element's new position:
-//     e.style.top = (elmnt.offsetTop - pos2) + "px";
-//     e.style.left = (elmnt.offsetLeft - pos1) + "px";
-// }
-
-let rose = document.querySelector("#rose");
-let tulip = document.querySelector("#tulip");
-let daisy = document.querySelector("#daisy");
-let lily = document.querySelector("#lily");
-let poppy = document.querySelector("#poppy");
+let menu = [...document.querySelectorAll(".menuItem")];
+let draggable = document.querySelector("#draggable");
+let canvas = document.querySelector("#canvas");
+let flowers = document.querySelectorAll('#rose, #tulip, #daisy, #lily, #poppy');
 
 function dragElement(elmnt) {
     let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onpointerdown = dragMouseDown;
+    let originalElmnt = elmnt.cloneNode(true);
+    let isCloned = false;
 
-    let count = [0];
+    function createClone(flower) {
+        return flower.cloneNode(true);
+    }
 
     function dragMouseDown(e) {
         e = e || window.event;
         e.preventDefault();
+        if (!isCloned) {
+            const clone = createClone(elmnt);
+            draggable.appendChild(clone);
+            dragElement(clone);
+            isCloned = true;
+        }
         // get the mouse cursor position at startup:
         pos3 = e.clientX;
         pos4 = e.clientY;
@@ -50,8 +29,6 @@ function dragElement(elmnt) {
         // call a function whenever the cursor moves:
         document.onpointermove = elementDrag;
         console.log('mouseDown');
-        count[0]++;
-        console.log(count);
     }
 
     function elementDrag(e) {
@@ -67,48 +44,81 @@ function dragElement(elmnt) {
         // console.log(`pos3: ${pos3}`)
         // set the element's new position:
         elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - (pos1)) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
         if (pos3 >= 10) {
-            switch (elmnt) {
-                case rose:
-                    elmnt.innerHTML = '<img src="../assets/roseF.png" id="item">';
+            switch (elmnt.id) {
+                case "rose":
+                    elmnt.innerHTML = '<img class="flower" src="../assets/roseF.png" id="item">';
+                    elmnt.style.width = '150px';
+                    elmnt.style.overflow = 'hidden';
+                    console.log("I work 2");
+                    break;
+                case "tulip":
+                    elmnt.innerHTML = '<img src="../assets/tulip.png" id="item">';
                     elmnt.style.width = '150px';
                     break;
-                case tulip:
-                    elmnt.innerHTML = '<img src="../assets/tulip.png" id="item">';
+                case "daisy":
+                    elmnt.innerHTML = '<img src="../assets/daisyF.png" id="item">';
+                    elmnt.style.width = '150px';
                     break;
-                case daisy:
-                    elmnt.innerHTML = '<img src="../assets/daisy.png" id="item">';
-                    break;
-                case lily:
+                case "lily":
                     elmnt.innerHTML = '<img src="../assets/lily.png" id="item">';
+                    elmnt.style.width = '150px';
                     break;
-                case poppy:
+                case "poppy":
                     elmnt.innerHTML = '<img src="../assets/poppy.png" id="item">';
+                    elmnt.style.width = '150px';
                     break;
             }
         }
     }
+    // function closeDragElement() {
+    //     // stop moving when mouse button is released:
+    // document.onpointerup = null;
+    // document.onpointermove = null;
+    //     const canvas = document.querySelector('#canvas');
+    //     canvas.append(elmnt);
 
+    //     const clone = elmnt.cloneNode(true);
+    //     const draggable = document.querySelector('#draggable');
+    //     draggable.append(clone);
+    //     dragElement(clone);
+    //     // const item = document.createElement();
+    //     // item.innerHTML = '<span id="mydiv"><img id="icon" src="../assets/rose.png"><p>Rose</p>'
+    //     console.log("dropped");
+    //     // const selection = document.querySelector("#mydiv");
+    //     // const item = document.createElement('img');
+    //     // item.setAttribute("id", "mydiv");
+    //     // item.src = '../assets/rose.png';
+    //     // selection.parentNode.replaceChild(item, selection);
+    // }
     function closeDragElement() {
-        // stop moving when mouse button is released:
         document.onpointerup = null;
         document.onpointermove = null;
-        const canvas = document.querySelector('#canvas');
         canvas.append(elmnt);
-        // const item = document.createElement();
-        // item.innerHTML = '<span id="mydiv"><img id="icon" src="../assets/rose.png"><p>Rose</p>'
-        console.log("dropped");
-        // const selection = document.querySelector("#mydiv");
-        // const item = document.createElement('img');
-        // item.setAttribute("id", "mydiv");
-        // item.src = '../assets/rose.png';
-        // selection.parentNode.replaceChild(item, selection);
-    }
-}
-dragElement(rose);
-dragElement(tulip);
-dragElement(daisy);
-dragElement(lily);
-dragElement(poppy);
+        let zIndex = elmnt.style.zIndex;
+        zIndex = -98;
+        zIndex --;
+        // randomizes z-index but it's kind of annoying, keeping jic
+        // const zIndex = Math.floor(Math.random() * 98) + 1;
+        // elmnt.style.zIndex = zIndex;
+        //     const otherFlowers = canvas.querySelectorAll('.flower');
+        //     let maxZIndex = -1;
+        //     otherFlowers.forEach(flower => {
+        //         const zIndex = parseInt(flower.style.zIndex);
+        //         if (zIndex && zIndex > maxZIndex) {
+        //             maxZIndex = zIndex;
+        //         }
+        //     });
+        //     elmnt.style.zIndex = (maxZIndex - 1).toString();
+        // elmnt.style.zIndex = zIndex
 
+        }
+
+        elmnt.onpointerdown = dragMouseDown;
+
+    }
+
+    flowers.forEach(flower => {
+        dragElement(flower);
+    });
