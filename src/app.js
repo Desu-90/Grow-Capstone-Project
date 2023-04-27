4bd0d1764827a40fe9849b430cc98b8e6c0935b7
Desu-90 <dolmedo837@gmail.com>
2023-04-26 23:14:23 -0400

    Great progress done tonight, the main app page is almost completed, missing the info tooltip and then finishing touches on the downloadable image and text for the letter

--- src/app.js
+++ src/app.js
@@ -1,16 +1,22 @@
-let menu = [...document.querySelectorAll(".menuItem")];
-let draggable = document.querySelector("#draggable");
-let canvas = document.querySelector("#canvas");
-let flowers = document.querySelectorAll('#rose, #tulip, #daisy, #lily, #poppy');
 
+let draggable = document.querySelector("#draggable");
+let canvas = document.querySelector("#canvas-content");
+let flowers = document.querySelectorAll('#anemone, #aspen, #babys, #bell, #birch, #calla, #carnation, #daisy, #daphodile, #delphinium, #green, #hydrangea, #lilly, #merigold, #million, #palma, #poppy, #rose, #sunflower, #tulip, #violet');
+let trash = document.querySelector('#trash');
+const exportBtn = document.querySelector('#send');
 function dragElement(elmnt) {
     let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     // otherwise, move the DIV from anywhere inside the DIV:
-    let originalElmnt = elmnt.cloneNode(true);
     let isCloned = false;
 
     function createClone(flower) {
-        return flower.cloneNode(true);
+        const clone = flower.cloneNode(true);
+        if (flower.nextSibling) {
+            flower.parentNode.insertBefore(clone, flower.nextSibling);
+        } else {
+            flower.parentNode.appendChild(clone);
+        }
+        return clone;
     }
 
     function dragMouseDown(e) {
@@ -18,13 +24,19 @@ function dragElement(elmnt) {
         e.preventDefault();
         if (!isCloned) {
             const clone = createClone(elmnt);
-            draggable.appendChild(clone);
+
             dragElement(clone);
             isCloned = true;
         }
+        elmnt.style.position = "absolute";
         // get the mouse cursor position at startup:
         pos3 = e.clientX;
         pos4 = e.clientY;
+        elmnt.style.top = (pos4 - (elmnt.offsetHeight / 2)) + "px";
+        elmnt.style.left = (pos3 - (elmnt.offsetWidth / 2)) + "px";
+
+        canvas.appendChild(elmnt);
+
         document.onpointerup = closeDragElement;
         // call a function whenever the cursor moves:
         document.onpointermove = elementDrag;
@@ -43,59 +55,90 @@ function dragElement(elmnt) {
         pos4 = e.clientY;
         // console.log(`pos3: ${pos3}`)
         // set the element's new position:
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        elmnt.style.top = (pos4 - (elmnt.offsetHeight / 2)) + "px";
+        elmnt.style.left = (pos3 - (elmnt.offsetWidth / 2)) + "px";
         if (pos3 >= 10) {
+
             switch (elmnt.id) {
-                case "rose":
-                    elmnt.innerHTML = '<img class="flower" src="../assets/roseF.png" id="item">';
-                    elmnt.style.width = '150px';
-                    elmnt.style.overflow = 'hidden';
-                    console.log("I work 2");
+                case "anemone":
+                    elmnt.innerHTML = '<img id="item" src="../assets/full_flowers/anemone.png">';
                     break;
-                case "tulip":
-                    elmnt.innerHTML = '<img src="../assets/tulip.png" id="item">';
-                    elmnt.style.width = '150px';
+                case "aspen":
+                    elmnt.innerHTML = '<img id="item" src="../assets/full_flowers/aspen.png">';
+                    break;
+                case "babys":
+                    elmnt.innerHTML = '<img id="item" src="../assets/full_flowers/babys.png">';
+                    break;
+                case "bell":
+                    elmnt.innerHTML = '<img id="item" src="../assets/full_flowers/bell.png">';
+                    break;
+                case "birch":
+                    elmnt.innerHTML = '<img id="item" src="../assets/full_flowers/birch.png" >';
+                    break;
+                case "calla":
+                    elmnt.innerHTML = '<img id="item" src="../assets/full_flowers/calla.png" >';
+                    break;
+                case "carnation":
+                    elmnt.innerHTML = '<img id="item" src="../assets/full_flowers/carnation.png" >';
+                    break;
+                case "daphodile":
+                    elmnt.innerHTML = '<img id="item" src="../assets/full_flowers/daffodile.png" >';
                     break;
                 case "daisy":
-                    elmnt.innerHTML = '<img src="../assets/daisyF.png" id="item">';
-                    elmnt.style.width = '150px';
+                    elmnt.innerHTML = '<img id="item" src="../assets/full_flowers/daisy.png">';
                     break;
-                case "lily":
-                    elmnt.innerHTML = '<img src="../assets/lily.png" id="item">';
-                    elmnt.style.width = '150px';
+                case "delphinium":
+                    elmnt.innerHTML = '<img id="item" src="../assets/full_flowers/delphinium.png">';
+                    break;
+                case "green":
+                    elmnt.innerHTML = '<img id="item" src="../assets/full_flowers/green.png">';
+                    break;
+                case "hydrangea":
+                    elmnt.innerHTML = '<img id="item" src="../assets/full_flowers/hydrangea.png">';
+                    break;
+                case "lilly":
+                    elmnt.innerHTML = '<img id="item" src="../assets/full_flowers/lilly.png">';
+                    break;
+                case "merigold":
+                    elmnt.innerHTML = '<img id="item" src="../assets/full_flowers/merigold.png">';
+                    break;
+                case "million":
+                    elmnt.innerHTML = '<img id="item" src="../assets/full_flowers/million.png">';
+                    break;
+                case "palma":
+                    elmnt.innerHTML = '<img id="item" src="../assets/full_flowers/palma.png">';
                     break;
                 case "poppy":
-                    elmnt.innerHTML = '<img src="../assets/poppy.png" id="item">';
-                    elmnt.style.width = '150px';
+                    elmnt.innerHTML = '<img id="item" src="../assets/full_flowers/poppy.png">';
+                    break;
+                case "rose":
+                    elmnt.innerHTML = '<img id="item" src="../assets/full_flowers/rose.png">';
+                    break;
+                case "sunflower":
+                    elmnt.innerHTML = '<img id="item" src="../assets/full_flowers/sunflower.png">';
+                    break;
+                case "tulip":
+                    elmnt.innerHTML = '<img id="item" src="../assets/full_flowers/tulip.png">';
+                    break;
+                case "violet":
+                    elmnt.innerHTML = '<img id="item" src="../assets/full_flowers/violet.png">';
                     break;
             }
         }
     }
-    // function closeDragElement() {
-    //     // stop moving when mouse button is released:
-    // document.onpointerup = null;
-    // document.onpointermove = null;
-    //     const canvas = document.querySelector('#canvas');
-    //     canvas.append(elmnt);
 
-    //     const clone = elmnt.cloneNode(true);
-    //     const draggable = document.querySelector('#draggable');
-    //     draggable.append(clone);
-    //     dragElement(clone);
-    //     // const item = document.createElement();
-    //     // item.innerHTML = '<span id="mydiv"><img id="icon" src="../assets/rose.png"><p>Rose</p>'
-    //     console.log("dropped");
-    //     // const selection = document.querySelector("#mydiv");
-    //     // const item = document.createElement('img');
-    //     // item.setAttribute("id", "mydiv");
-    //     // item.src = '../assets/rose.png';
-    //     // selection.parentNode.replaceChild(item, selection);
-    // }
     function closeDragElement() {
         document.onpointerup = null;
         document.onpointermove = null;
-        canvas.append(elmnt);
+        const draggableRect = elmnt.getBoundingClientRect();
+        const droppableRect = trash.getBoundingClientRect();
+
+        if (draggableRect.left < droppableRect.right &&
+            draggableRect.right > droppableRect.left &&
+            draggableRect.top < droppableRect.bottom &&
+            draggableRect.bottom > droppableRect.top) {
+            elmnt.remove();
+        }
         let zIndex = elmnt.style.zIndex;
         zIndex = -98;
         zIndex--;
@@ -116,15 +159,21 @@ function dragElement(elmnt) {
     }
 
     elmnt.onpointerdown = dragMouseDown;
-
 }
-
-
-
-
-
-
 
 flowers.forEach(flower => {
     dragElement(flower);
+});
+
+
+
+exportBtn.addEventListener('click', () => {
+    // Use html2canvas to export the div to an image
+    html2canvas(document.querySelector("#canvas-content")).then(canvas => {
+        // Create an Image element and set its src to the canvas data URL
+        var img = new Image();
+        img.src = canvas.toDataURL();
+        // Append the Image element to the body of the page
+        document.body.appendChild(img);
+    });
 });
